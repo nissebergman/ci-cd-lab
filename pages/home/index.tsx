fd482c95c32c207c4b57f86e7bf5771529c089cb
nissebergman <senibe01@parksandresorts.com>
2025-03-30 18:52:31 +0200

    devpush

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home Dev!</h1>
       <Link href="/home/about">About</Link>
     </div>
   );
